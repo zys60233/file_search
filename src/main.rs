94d68e0fa7c019acca7fe51150705c791c0a798f
妖刀 <zysDevelopor@163.com>
2025-04-08 15:47:27 +0000

    基本功能

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    let name: String = String::from("zys");
-    println!("Hello, world! {}", name);
+    //环境参数
+    let args: Vec<String> = env::args().collect();
+
+    //要查询的字符串
+    let query = &args[1];
+
+    //文件名
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
+    //文件内容
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}")
 }
