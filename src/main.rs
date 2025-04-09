b7fb576f3b629234dcc36bd7a873fea45126fc6a
妖刀 <zysDevelopor@163.com>
2025-04-09 01:27:50 +0000

    重定向错误信息的输出

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,16 @@ fn main() {
 
     //配置变量
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
 
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    eprintln!("Searching for {}", config.query);
+    eprintln!("In file {}", config.file_path);
 
     if let Err(e) = file_search::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     };
 }
