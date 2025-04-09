983759be0c02740700754fae7d88e9755b4c66e5
妖刀 <zysDevelopor@163.com>
2025-04-09 01:51:13 +0000

    使用迭代器来改进程序（可选）

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
-//官方库
 use std::env;
 use std::process;
 
-//项目库
 use file_search::Config;
 
-
 fn main() {
-    //环境参数
-    let args: Vec<String> = env::args().collect();
-
-    //配置变量
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
 
