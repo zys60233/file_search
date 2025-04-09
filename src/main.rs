7d9f5debd101621d115b6c55ce17153630b5fba9
妖刀 <zysDevelopor@163.com>
2025-04-09 00:39:48 +0000

    增加模块化和错误处理

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
+//官方库
 use std::env;
-use std::fs;
+use std::process;
+
+//项目库
+use file_search::Config;
+
 
 fn main() {
     //环境参数
     let args: Vec<String> = env::args().collect();
 
-    //要查询的字符串
-    let query = &args[1];
+    //配置变量
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
 
-    //文件名
-    let file_path = &args[2];
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    //文件内容
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}")
+    if let Err(e) = file_search::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    };
 }
